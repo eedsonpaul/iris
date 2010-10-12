675f11545efbc48414c606c48ed4cb03304ef7f9
edsonpaul <PC-@.(none)>
2010-10-13 04:09:54 +0800

    changed footer UI

--- admin_footer.php
+++ admin_footer.php
@@ -5,12 +5,15 @@
     <div id="footer_contents">
       <br/>
       <b><a href="links/OnlineHelp.html" target="_new">Online Help</a> | <a href="links/aup.php" target="_new">AUP</a> | <a href="links/privacy.php" target="_new">Privacy</a> | <a href="links/disclaimer.php" target="blank">Disclaimer</a> | <a href="links/contact.php" target="_new">Contact Webmaster</a>&nbsp;</b>
-      </br><br/><br/>
+      </br><br/>
 
-      Copyright &copy; 2010-2012. All Rights Reserved.<br>
+      <p>
+      <span class="footer_allrights">Copyright &copy; 2010-2012. All Rights Reserved.&nbsp;</span><br>
       Office of the College Registrar, University of the Philippines Cebu College, Lahug, Cebu City 6000&nbsp;<br/>
       Phone/Fax: +63 (33) 315 8556 | Email: ourupcebu@gmail.com&nbsp;
       <br/><br/><br/>
+      </p>
+      
       <p>
       <span class="white"><b><font color="white">University of the Philippines</b>  <a href="http://up.edu.ph" target="_blank"><font color="white">Diliman</font></a> | <a href="http://officialweb.upm.edu.ph/" target="_blank"><font color="white">Manila</font></a> | <a href="http://www.uplb.edu.ph/" target="_blank"><font color="white">Los Banos</font></a> | <a href="http://www.upb.edu.ph/" target="_blank"><font color="white">Baguio</font></a> | <a href="http://www.upv.edu.ph/" target="_blank"><font color="white">Visayas</font></a> | <a href="http://www.upmin.edu.ph/" target="_blank"><font color="white">Mindanao</font></a> | <a href="http://www.upou.edu.ph/" target="_blank"><font color="white">Open University</font></a></br><br/>&nbsp;</span>
       </p><br/> 
